0a32ed665c7e829f9c6c2a31caf6031fe77e6fb5
David Ludtke <36012@superior.k12.wi.us>
2026-06-12 08:06:06 -0500

    Added Constants.java for constants and added simgui-ds.json for sim settings

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,53 +1,18 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
-/**
- * The methods in this class are called automatically corresponding to each mode, as described in
- * the TimedRobot documentation. If you change the name of this class or the package after creating
- * this project, you must also update the Main.java file in the project.
- */
-public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
-  public Robot() {}
+class Robot extends TimedRobot {
+  public Robot() {
+    super(1.0 / 50.0);
+  }
 
-  @Override
-  public void robotPeriodic() {}
+  @Override public void robotPeriodic() {
+    CommandScheduler.getInstance().run();
+  }
 
-  @Override
-  public void autonomousInit() {}
+  @Override public void simulationInit() {}
 
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
-  public void teleopInit() {}
-
-  @Override
-  public void teleopPeriodic() {}
-
-  @Override
-  public void disabledInit() {}
-
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void testInit() {}
-
-  @Override
-  public void testPeriodic() {}
-
-  @Override
-  public void simulationInit() {}
-
-  @Override
-  public void simulationPeriodic() {}
+  @Override public void robotInit() {}
 }
